0439b77d9c3d5e19471448d750832f10cdff3f9d
shiva04 <shivaprasadmullapudi@gmail.com>
2024-07-31 16:54:00 -0400

    changes in env file

--- env.py
+++ env.py
@@ -4,10 +4,16 @@ folders=input("enter the folder name list with spaces in between : ").split(" ")
 
 for folder in folders:
     try:
-        file=os.listdir(folder)
+        files=os.listdir(folder)
     except FileNotFoundError:
         print(f"Wrong folder name {folder}, Enter a valid folder name")
         continue
     
-print(file)
+    for file in files:
+        print(file)
+        
+        
+        
+        
+        
 # print(os.getenv("password"))
